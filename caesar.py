b3422b67dac59db856edacbd9ef38194c061822f
amarylis <aa.valentin00@gmail.com>
2024-01-29 20:52:41 -0500

    adding char barriers

--- caesar.py
+++ caesar.py
@@ -1,20 +1,30 @@
 import sys
 
 def encrypt(message, k):
-    alpha = []
+    alpha_encrypt = []
     for x in message:
         numer = ord(x)
         char = numer + k
-        if
-        alpha.append(chr(char))
-    return alpha
+        if char < 97:
+            char += 26
+        elif char > 122:
+            char -= 26
+        alpha_encrypt.append(chr(char))
+        encrypt_string = ''.join(alpha_encrypt)
+    return encrypt_string
 
 def decrypt(message, k):
+    alpha_decrypt = []
     for x in message:
         numer = ord(x)
         char = numer - k
-        print(chr(char))
-    return
+        if char < 97:
+            char += 26
+        elif char > 122:
+            char -= 26
+        alpha_decrypt.append(chr(char))
+        decrypt_string = ''.join(alpha_decrypt)
+    return decrypt_string
 
 if __name__ == "__main__":
     # take in first arg as word
